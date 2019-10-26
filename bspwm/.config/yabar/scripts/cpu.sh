03de1bb65089f3963aed37a276f7c8d0ce2a5212
tunnelshade <tunnelshade@gmail.com>
2019-10-26 22:02:13 +0530

    Yabar and cpu cnf fix

--- bspwm/.config/yabar/scripts/cpu.sh
+++ bspwm/.config/yabar/scripts/cpu.sh
@@ -1,1 +1,1 @@
-echo "  " $(LC_ALL="en_US.UTF-8" mpstat -P ALL | tail -n 4 | awk '{ printf " %02.0f" , 100-$13 }')
+echo "  " $(LC_ALL="en_US.UTF-8" mpstat -P ALL | awk '/ [0-9][0-9]? /{ printf " %02.0f" , 100-$13 }')
